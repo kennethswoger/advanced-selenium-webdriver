8e7e5b7df9d9ab59ce7678ae1e0ddae5a5a8ce08
Kenneth Swoger <krswoger@gmail.com>
2020-11-02 08:02:36 -0600

    update imports and add extends

--- BEGINNING/advanced-selenium-webdriver/src/test/java/com/herokuapp/theinternet/loginpagetests/NegativeLoginTests.java
+++ BEGINNING/advanced-selenium-webdriver/src/test/java/com/herokuapp/theinternet/loginpagetests/NegativeLoginTests.java
@@ -1,46 +1,15 @@
 package com.herokuapp.theinternet.loginpagetests;
 
+import com.herokuapp.theinternet.base.BaseTest;
+import com.herokuapp.theinternet.base.TestUtilities;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class NegativeLoginTests {
+public class NegativeLoginTests extends TestUtilities {
 
-	WebDriver driver;
-
-
-	@Parameters({ "browser" })
-	@BeforeMethod
-	private void setUp(@Optional("chrome") String browser) {
-		// Create driver
-		System.out.println("Create driver: " + browser);
-
-		switch (browser) {
-		case "chrome":
-			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-			driver = new ChromeDriver();
-			break;
-
-		case "firefox":
-			System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
-			driver = new FirefoxDriver();
-			break;
-
-		default:
-			System.out.println("Do not know how to start: " + browser + ", starting chrome.");
-			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-			driver = new ChromeDriver();
-			break;
-		}
-		driver.manage().window().maximize();
-	}
 
 
 	@Parameters({ "username", "password", "expectedMessage" })
@@ -70,11 +39,4 @@ public class NegativeLoginTests {
 						+ expectedErrorMessage + "\nactualErrorMessage: " + actualErrorMessage);
 	}
 
-
-	@AfterMethod
-	private void tearDown() {
-		System.out.println("Close driver");
-		// Close browser
-		driver.quit();
-	}
 }
