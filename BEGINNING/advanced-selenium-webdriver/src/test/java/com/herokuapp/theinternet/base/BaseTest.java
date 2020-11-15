f3d4d16910c0837b25f63b5a2e81c7fa5475f2ed
Kenneth Swoger <krswoger@gmail.com>
2020-11-15 11:16:16 -0600

    updates for logger

--- BEGINNING/advanced-selenium-webdriver/src/test/java/com/herokuapp/theinternet/base/BaseTest.java
+++ BEGINNING/advanced-selenium-webdriver/src/test/java/com/herokuapp/theinternet/base/BaseTest.java
@@ -1,8 +1,9 @@
 package com.herokuapp.theinternet.base;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
@@ -11,37 +12,22 @@ import org.testng.annotations.Parameters;
 public class BaseTest {
 
     protected WebDriver driver;
-
+    protected Logger log;
 
     @Parameters({ "browser" })
     @BeforeMethod(alwaysRun = true)
-    public void setUp(@Optional("chrome") String browser) {
-        // Create driver
-        System.out.println("Create driver: " + browser);
+    public void setUp(@Optional("chrome") String browser, ITestContext ctx) {
+        String testName = ctx.getCurrentXmlTest().getName();
+        log = LogManager.getLogger(testName);
 
-        switch (browser) {
-            case "chrome":
-                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-                driver = new ChromeDriver();
-                break;
+        BrowserDriverFactory factory = new BrowserDriverFactory(browser, log);
+        driver = factory.createDriver();
 
-            case "firefox":
-                System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
-                driver = new FirefoxDriver();
-                break;
-
-            default:
-                System.out.println("Do not know how to start: " + browser + ", starting chrome.");
-                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-                driver = new ChromeDriver();
-                break;
-        }
-        driver.manage().window().maximize();
     }
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
-        System.out.println("Close driver");
+        log.info("Close driver");
         // Close browser
         driver.quit();
     }
